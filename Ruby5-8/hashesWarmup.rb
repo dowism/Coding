863c52597b8a00c8a1d5a9edb9d681e406518a52
Seth Dow <sdow@sbacademy.org>
2016-12-15 15:51:18 -0800

    Changes

--- Ruby5-8/hashesWarmup.rb
+++ Ruby5-8/hashesWarmup.rb
@@ -3,9 +3,8 @@ This warmup is for use after introToHashes.rb
 
 I want you to make an object(a hash) that lists what training you do each day of the week. Mine would look like this
 
+YOU MUST USE A DIFFERENT SYNTAX THAN THIS, this is object literal, I want you to use constructor syntax. which starts by making an object with Hash.new and continues by adding keys one at a time. 
 
-
-=end
 
 training = {
   monday: "weight lifting after school",
@@ -14,4 +13,8 @@ training = {
   thursday: "Short nordic ski",
   friday: "Free ski or nordic after school depending on conditions",
   saturday:["kayak if possible","Backcountry","nordic","Workout"]
+  sunday:
 }
+
+
+=end
